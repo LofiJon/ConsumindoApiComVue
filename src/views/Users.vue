9808f2b84ae9c07070abdf6dbc077a635a1fae67
Jonathan Malagueta <jonathan@mbconsultoria.com>
2021-03-09 13:10:34 -0400

    correção tabela

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -9,9 +9,10 @@
               <table class="table table-dark mt-4">
                   <thead>
                       <tr>
-                                <th scope="col">id</th>
+                                
                                 <th scope="col">nome</th>
                                 <th scope="col">email</th>
+                                <th scope="col">telefone</th>
                                 
                                 
                        </tr>
@@ -32,7 +33,7 @@
       <div class="row">
           <div class="col-md-2"></div>
          
-          <div class="col-md-8 align-self">
+          <div class="col-md-8 col-sm-12  align-self">
                <h2>Inserir dados </h2>
               <input type="text" v-model="newUser" class="form-control">
               <input type="text" v-model="newEmail" class="form-control mt-2">
